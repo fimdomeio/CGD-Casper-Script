6cde771ea7cbbd5f0e0c440ec3aaf3292f4139ff
Alexandre Castro <alexandre@homem-arvore.com>
2012-07-25 17:40:55 +0100

    added user and password as arguments

--- cgdmovimentos.js
+++ cgdmovimentos.js
@@ -11,37 +11,65 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-var casper = require("casper").create();
+var casper = require("casper").create({
+//Enable in development
+//	verbose: true,
+//	logLevel: "debug"
+});
+var contractnr; // Will hold contract number
+var password; // Will hold the password
 
+
+if(casper.cli.has(0) && casper.cli.has(1)){
+	contractnr = casper.cli.get(0);
+	password = casper.cli.get(1);
+	password = password.split(""); //Split into array since password caracters are sent individually
+}else {
+	casper.echo('Usage is "casperjs cgdmovimentos.js ‹contract-nr› ‹password›');
+	casper.exit();
+}
+
+
+//Inserts contract Number on the first page
 casper.start('https://www.cgd.pt/Pages/Default.aspx', function() {
+   this.echo(contractnr);
    this.fill('.login_outer_box form', {
-      'USERNAME':    'XXXXXXX', //Contract number (todo: make it recieve an argument)
-   }, true);
+      'USERNAME': contractnr.toString()
+	}, true);
 });
 
 casper.then(function(){
-	 this.click('#j_id36');
-	 this.clickLabel('X', 'div'); //Password (todo make it a recieve argument)
-	 this.clickLabel('X', 'div');
-	 this.clickLabel('X', 'div');
-	 this.clickLabel('X', 'div');
-	 this.clickLabel('X', 'div');
-	 this.clickLabel('X', 'div');
-	 this.click('.VirtualKbd_Enter_tecla input');
+	//this.click('#j_id36');
+	var c = password.length;
+	this.echo(c);
+	for(var i = 0; i < c; i++){
+		//click each password character on the virtual keyboard
+		this.clickLabel(password[i], 'div'); 
+	}
+	this.click('.VirtualKbd_Enter_tecla input');
 });
 
-casper.waitUntilVisible('.mensBoas',function(){ // Wait until login is completed
+//Waits until login is completed
+casper.waitUntilVisible('.mensBoas',function(){
 },
 function(){
-	this.echo('Não Consigo ligar ao servidor');	
+	//
+	// TODO has password failed was it a problem with the cgd server?
+	//
+	this.echo("Can't authenticate to server.");	
 },
 15000);
 
 //Go to the movimentos Page
+//
+// TODO allow to specify different date ranges with arguments
+//
 casper.thenOpen('https://caixadirectaonline.cgd.pt/cdo/private/contasaordem/consultaSaldosMovimentos.seam', function(){
 	var results = this.fetchText('.mgtop tr .texttable:nth-child(-n+4)'); //get results
 	//
-	// TODO Get description from title and not from content since it's more complete
+	// TODO Get description from title attribute instead of the content since it's more complete
+	//
+	// TODO Format the results as json for easier manipulation. Configurable by console argument
 	//
 
 	results = results.replace(/\t/g, "");
@@ -68,7 +96,7 @@ casper.thenOpen('https://caixadirectaonline.cgd.pt/cdo/private/contasaordem/cons
 	this.echo(output);
 });
 
-// ADD LOGOUT
+// ADD LOGOUT. I think it's redundant since apparently phantom doesn't save anything in cache
 
 casper.run(function() {
 		    this.exit();
